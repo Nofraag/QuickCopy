34b07d3fbe1fe77d65501c70ae3cbf2cf6ccd4ae
Nofraag <fra.alacc@gmail.com>
2026-03-28 19:21:35 +0100

    updated name to QuickClone

--- src/main/java/name/Nofra/QuickClone/QuickClone.java
+++ src/main/java/name/Nofra/QuickClone/QuickClone.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QuickClone implements ClientModInitializer {
-    public static final String MOD_ID = "creativesmind";
+    public static final String MOD_ID = "quickclone";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final QuickCloneConfig CONFIG = QuickCloneConfig.createAndLoad();
 
